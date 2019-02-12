22a14490d17f71b55e2c8ff016ba47a0c55140a6
Yami Thalyssa <thalyssa2.0@hotmail.com>
2019-02-12 14:03:25 -0300

    Update bd.java

--- bd.java
+++ bd.java
@@ -1,1 +1,33 @@
-//Classe criada para armazenar os recursos do sistema
+import java.util.ArrayList;
+
+public class bd {
+    //FUNCIONÁRIOS DO CLUBE
+    //Presidente
+    private person president;
+
+    //Médicos
+    public ArrayList<doctor> doctors = new ArrayList<doctor>();
+
+    //Técnico
+    public ArrayList<person> techs = new ArrayList<person>();
+
+    //Personal trainers
+    public ArrayList<person> trainers = new ArrayList<person>();
+
+    //Drivers
+    public ArrayList<driver> drivers = new ArrayList<driver>();
+
+    //Chefs
+    public ArrayList<person> chefs = new ArrayList<person>();
+
+    //Lawyers
+    public ArrayList<person> lawyers = new ArrayList<person>();
+
+    //Players
+    public ArrayList<player> splayers = new ArrayList<player>();
+
+    //FAN ASSOCIATES
+
+    //RESOURCES
+}
+
